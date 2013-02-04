8cfdcd0d938a2cf24da800cb5b25cc64abd96f34
Jérémy Lecerf <redpist.com@gmail.com>
2013-02-04 19:10:08 +0100

    minor modification: identation

--- js/modal/modal.js
+++ js/modal/modal.js
@@ -19,17 +19,15 @@
         // closeTrigger: '.close',
         // successTrigger: 'form',
       };
-
       options = $.extend(defaults, options);
 
       var overlay = $('<div class="rmodal-overlay"></div>');
-        
       $("body").append(overlay);
 
       return this.each(function() {
         var modal = $(this).attr('href');
-      var modalSelector = modal;
-      var modalUrl = modal;
+        var modalSelector = modal;
+        var modalUrl = modal;
 
         if (modal[0] === '#') {
           $(modalSelector).addClass('rmodal-modal');
@@ -38,18 +36,20 @@
         }
 
         $(this).click(function(event) {
+
           function openModal(modalSelector, closeCallback) {
             $(overlay).css('display', 'block');
             $(modalSelector).css({
               'margin-top': 0,
               'margin-left': -($(modalSelector).outerWidth() / 2),
               'display': 'block'});
-            var closeModal = function() {
+
+            function closeModal() {
               closeCallback();
               $(overlay).css('display', 'none');
-            };                  
-            $(overlay).click(closeModal);
+            };
 
+            $(overlay).click(closeModal);
             $(document).keyup(function(event) {
               if (event.which == 27) {
                 console.log('close : ' + modalSelector);
@@ -58,24 +58,24 @@
               }
             });
           };
-          if (modal[0] === '#') {
-            openModal(modalSelector, function() {
-              $(modalSelector).css('display', 'none');
-            });
-        } else {
-        modalSelector = $('<div class="rmodal-modal"></div>');
-        $("body").append(modalSelector);
-            $.get(modalUrl, function(data) {
-              $(modalSelector).html(data);
-            }, 'html');
-            openModal(modalSelector, function() {
-              $(modalSelector).remove();
-            });
-          }
 
-          return false;
+        if (modal[0] === '#') {
+          openModal(modalSelector, function() {
+            $(modalSelector).css('display', 'none');
+          });
+        } else {
+          modalSelector = $('<div class="rmodal-modal"></div>');
+          $("body").append(modalSelector);
+          $.get(modalUrl, function(data) {
+            $(modalSelector).html(data);
+          }, 'html');
+          openModal(modalSelector, function() {
+            $(modalSelector).remove();
+          });
+        }
+        return false;
         });
       });
     }
-  });    
+  });  
 })(jQuery);
